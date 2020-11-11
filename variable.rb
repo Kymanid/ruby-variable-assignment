9095f1fcbe51a94d34993af687faa02c30a8fb87
Kymani Davis <Kymanidavis@gmail.com>
2020-11-11 00:25:31 -0500

    Done.

--- variable.rb
+++ variable.rb
@@ -2,4 +2,7 @@
  # give it the value of the string "Hello World"
  greeting = "Hello World"
 
-practicing saving
+
+ # learn submit - will submit project
+ # learn test - will run the tests for the project
+ # learn save - saving the ccurrent code to github
